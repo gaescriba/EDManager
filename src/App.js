e7fe70806a7b4db2063a2746b59f33d009cc8d29
gaescriba <gaescriba.trabajo@gmail.com>
2021-07-24 19:41:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Provider} from 'react-redux'
+import EquipoSeleccionado from './components/EquipoSeleccionado';
+import Jugadores from './components/Jugadores';
+import store from './store';
+import './styles/styles.scss'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}> 
+      <main>
+        <h1>EDmanager</h1>
+        <Jugadores/>
+        <EquipoSeleccionado/>
+      </main>
+    </Provider>
+   );
 }
-
+ 
 export default App;
